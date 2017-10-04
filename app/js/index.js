d00eb030e4658999aed648d04bc2066fe9557944
Eric W. Greene <eric@ericwgreene.com>
2017-10-04 10:24:51 -0600

    added code demos

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,9 @@
 (function(window, $, Backbone, _) {
 
+  Instructions: Take the Car table code from yesterday and wrap it in
+  a View Class. Instantiate the view class, pass in the cars, then 
+  render and display the cars.
+
   var pageHeader = new App.Views.PageHeader();
   $('main').append(pageHeader.render());
 
